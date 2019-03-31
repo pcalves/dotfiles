b097e5a28762098fee8622c488e2ba03847d08fa
Paulo Coelho Alves <paulo@pcalv.es>
2019-03-31 11:26:29 +0100

    Add script to push vimwiki changes

---
+++ scripts/vimwiki-push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# From: http://wiki.qiaoanran.com/vimwiki.html
+
+GIT=`which git`
+COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
+
+# convert wiki files to html files
+vim +VimwikiTabIndex +VimwikiAll2HTML +qall
+
+cd ~/vimwiki
+${GIT} add --all .
+${GIT} commit -am "Automated update on ${COMMIT_TIMESTAMP}"
+${GIT} push origin master
