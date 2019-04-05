215dc2e1df2a55f013de8a2f7c0dd2e26912f642
Paulo Coelho Alves <paulo@pcalv.es>
2019-04-05 14:47:07 +0100

    Move qutebrowser perf flags to config

--- skhd/open-qutebrowser.sh
+++ skhd/open-qutebrowser.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/Applications/qutebrowser.app/Contents/MacOS/qutebrowser --backend webengine --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers
+/Applications/qutebrowser.app/Contents/MacOS/qutebrowser --backend webengine
