d1db7ef7ce89a8d4af3a7c6f671d81175ca10fe2
Paulo Coelho Alves <paulo@pcalv.es>
2019-07-26 15:05:35 +0100

    Fix wal.sh to prevent Dock from dying

--- scripts/wal.sh
+++ scripts/wal.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-wal -i "$1" && osascript -e 'tell application "'$(ps ax | grep sicht | awk '{print $5}' | head -1 | cut -d/ -f3 | cut -d. -f1)'" to refresh' && ~/.chunkwmrc && kitty @ set-colors --all --configured ~/.cache/wal/colors-kitty.conf
+wal -n -i "$1" && osascript -e 'tell application "'$(ps ax | grep sicht | awk '{print $5}' | head -1 | cut -d/ -f3 | cut -d. -f1)'" to refresh' && ~/.chunkwmrc && kitty @ set-colors --all --configured ~/.cache/wal/colors-kitty.conf
+
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"$(cat "$HOME/.cache/wal/wal")\""
 
